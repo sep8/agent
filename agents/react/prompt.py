49f6a6e376d3d4e41ccc777e6bc0be3e6eb4ba13
Mingyang Han <ming8525@esri.com>
2023-07-13 17:53:01 +0800

    fix: rename mrkl to react

--- agents/react/prompt.py
+++ agents/react/prompt.py
@@ -1,5 +1,6 @@
 # flake8: noqa
 PREFIX = """Answer the following questions as best you can. You have access to the following tools:"""
+
 FORMAT_INSTRUCTIONS = """Use the following format:
 
 Question: the input question you must answer
@@ -10,6 +11,7 @@ Observation: the result of the action
 ... (this Thought/Action/Action Input/Observation can repeat N times)
 Thought: I now know the final answer
 Final Answer: the final answer to the original input question"""
+
 SUFFIX = """Begin!
 
 Question: {input}
